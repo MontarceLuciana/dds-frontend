cfc22b81c1e5d6c05d99e47b86986d702f0dcb91
sadsadasdsa <asdsadsad2@sistemas.frc.utn.edu.ar>
2024-05-31 17:19:52 -0300

    etapa 2

---
+++ dds-frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import {ArticulosFamilias} from './components/ArticulosFamilias';
+function App() {
+  return (
+    <>
+      <div className="divBody">
+          <ArticulosFamilias/>
+      </div>
+    </>
+  );
+}
+export default App;
